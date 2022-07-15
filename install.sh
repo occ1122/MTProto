5e759829f8a8082ddc8532d4bfed52b6e5f8122d
occ1122 <82438606+occ1122@users.noreply.github.com>
2022-07-16 00:25:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ TMPDIR=$(mktemp -d)
 trap 'echo Signal caught, cleaning up >&2; cd /; /bin/rm -rf "$TMPDIR"; exit 15' 1 2 3 15
 
 BIN=$TMPDIR/mtg.bin
-wget -qO - https://api.github.com/repos/cutelua/mtg-dist/releases/latest \
+wget -qO - https://api.github.com/repos/occ1122/MTProto/releases/latest \
 | grep browser_download_url | cut -d '"' -f 4 | wget -q -i - -O $BIN
 bash $BIN
 rm -rf $TMPDIR
